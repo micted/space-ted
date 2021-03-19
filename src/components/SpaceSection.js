776fb8cebd518690461890f5d2a0b0ecad1f0b56
micted <63680542+micted@users.noreply.github.com>
2021-03-20 01:36:38 +0300

    Update SpaceSection.js

--- src/components/SpaceSection.js
+++ src/components/SpaceSection.js
@@ -7,7 +7,7 @@ function SpaceSection() {
     return (
         <div className='space-container'>
             <video src="/videos/GALAXY.mp4" autoPlay loop muted/>            
-           <h1>Welcome to SPACE-TED</h1>
+           <h1>WELCOME to SPACE-TED</h1>
             <p>Explore the infinty stars!</p>
             <div className='space-btns'>
                 <Button 
