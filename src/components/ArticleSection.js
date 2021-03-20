8d52bd3bc617eb564cd1063a2cd354bb337fa109
micted <63680542+micted@users.noreply.github.com>
2021-03-20 18:39:16 +0300

    Update ArticleSection.js

--- src/components/ArticleSection.js
+++ src/components/ArticleSection.js
@@ -8,7 +8,7 @@ function ArticleSection() {
         <div className='articles-container'> 
       <h1>SPACE SCIENCE &amp; TECHNOLOGY</h1> 
         <video src="/videos/rocket.mp4" autoPlay loop muted/>    
-      <p1>Learn more about emerging technologies in the space industry</p1>
+      <p>Learn more about emerging technologies in the space industry</p>
     </div>
     )
 }
